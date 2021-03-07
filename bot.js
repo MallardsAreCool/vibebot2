9175f0eae35228ec2582cec2d8907872cd2852c2
DuckBoy <harry4234@hotmail.co.uk>
2021-03-07 17:03:43 +0000

    shit

--- bot.js
+++ bot.js
@@ -9,8 +9,6 @@ let JSONdata = JSON.parse(fs.readFileSync(JSONpath));
 
 client.login(TOKEN);
 
-var joinTime = Date.now();
-
 client.once('ready', () => {
 	console.info(`Logged in as ${client.user.tag}!`);
 	client.user.setActivity(`With People's Emotions`, {
@@ -21,75 +19,93 @@ client.once('ready', () => {
 client.on('voiceStateUpdate', (oldMember, newMember) => {
 	let newUserChannel = newMember.channelID;
 
-	if (newUserChannel !== null) {
-		if (JSONdata[newMember.id].inCall) return;
-		else {
-			console.log('join');
-
-			joinTime = Date.now();
-			JSONdata[newMember.id].inCall = true;
-			fs.writeFileSync(JSONpath, JSON.stringify(JSONdata, null, 2));
-		}
-	} else {
-		console.log('leave');
-
-		JSONdata[newMember.id].inCall = false;
-		fs.writeFileSync(JSONpath, JSON.stringify(JSONdata, null, 2));
-
-		if (!JSONdata[newMember.id]) {
-			JSONdata[message.author.id] = {
-				timeInVC: 0,
-				inCall: false,
-			};
-		}
-
-		JSONdata[newMember.id].timeInVC += Date.now() - joinTime;
-		fs.writeFileSync(JSONpath, JSON.stringify(JSONdata, null, 2));
-	}
+	if (newUserChannel === null || newUserChannel == newMember.guild.afkChannelID) leaveCall(newMember.id);
+	else joinCall(newMember.id);
 });
 
-function msToTime(duration) {
-	var milliseconds = parseInt((duration % 1000) / 100),
-		s = Math.floor((duration / 1000) % 60),
-		m = Math.floor((duration / (1000 * 60)) % 60),
-		h = Math.floor((duration / (1000 * 60 * 60)) % 24);
+function joinCall(memberID) {
+	if (JSONdata[memberID].inCall) return;
+	else {
+		console.log('join');
 
-	return h + ' Hours, ' + m + ' Minutes, ' + s + ' Seconds';
+		JSONdata[memberID].inCall = true;
+
+		updateTime(memberID);
+	}
+}
+
+function leaveCall(memberID) {
+	console.log('leave');
+
+	JSONdata[memberID].inCall = false;
+
+	updateTime(memberID);
+}
+
+function updateTime(memberID) {
+	if (!JSONdata[memberID]) {
+		JSONdata[memberID] = {
+			timeInVC: 0,
+			inCall: false,
+			joinTime: 0,
+		};
+	}
+
+	if (JSONdata[memberID].joinTime != 0) JSONdata[memberID].timeInVC += Date.now() - JSONdata[memberID].joinTime;
+
+	if (JSONdata[memberID].inCall) JSONdata[memberID].joinTime = Date.now();
+	else JSONdata[memberID].joinTime = 0;
+
+	fs.writeFileSync(JSONpath, JSON.stringify(JSONdata, null, 2));
+}
+
+function msToTime(duration) {
+	var s = Math.floor((duration / 1000) % 60),
+		m = Math.floor((duration / (1000 * 60)) % 60),
+		h = Math.floor((duration / (1000 * 60 * 60)) % 24),
+		d = Math.floor((duration / (1000 * 60 * 60 * 24)) % 7);
+
+	return [d, h, m, s];
+}
+
+function getUserFromMention(mention) {
+	if (!mention) return;
+
+	if (mention.startsWith('<@') && mention.endsWith('>')) {
+		mention = mention.slice(2, -1);
+
+		if (mention.startsWith('!')) {
+			mention = mention.slice(1);
+		}
+
+		return client.users.cache.get(mention);
+	}
 }
 
 client.on('message', (message) => {
-	if (message.content == 'time') {
-		message.channel.send(msToTime(JSONdata[message.author.id].timeInVC));
-	}
-	{
-		// let messageToLowercase = message.content.replace(/\s/g, '').toLowerCase();
-		// if (message.author.bot) return;
-		// if (message.channel.name.toLowerCase() == WORD) {
-		// 	if (!validMeat(messageToLowercase)) {
-		// 		if (!JSONdata[message.author.id]) {
-		// 			JSONdata[message.author.id] = {
-		// 				username: `${message.author.username}`,
-		// 				strikes: 0,
-		// 			};
-		// 		}
-		// 		JSONdata[message.author.id].strikes++;
-		// 		fs.writeFileSync(JSONpath, JSON.stringify(JSONdata, null, 2));
-		// 		console.log(JSONdata[message.author.id].strikes);
-		// 		let msg = `${message.author}, you fool. you absolute buffoon. you think you can challenge me in my own realm? you think you can rebel against my authority? you dare come into my house and upturn my dining chairs and spill coffee grounds in my Keurig? you thought you were safe in your chain mail armour behind that screen of yours. I will take these laminated wood floor boards and destroy you. I didn't want war. but i didn't start it.\n\nIt is illegal to say anything but **${WORD}** in this server.`;
-		// 		if (message.author.id == ownerID) {
-		// 			message.channel.send(msg);
-		// 		} else if (JSONdata[message.author.id].strikes == 1) {
-		// 			message.channel.send(msg + ` Strikes: ${JSONdata[message.author.id].strikes}/3`);
-		// 		} else if (JSONdata[message.author.id].strikes == 2) {
-		// 			message.channel.send(msg + ` __Strikes: ${JSONdata[message.author.id].strikes}/3__`);
-		// 		} else if (JSONdata[message.author.id].strikes == 3) {
-		// 			message.channel.send(msg + ` **__Strikes: ${JSONdata[message.author.id].strikes}/3__**`);
-		// 		} else if (JSONdata[message.author.id].strikes >= 4) {
-		// 			message.member.send(`https://discord.gg/b5jhFfKJWe`).then(() => {
-		// 				message.member.kick('VibeBot is disappointed in you.');
-		// 			});
-		// 		}
-		// 	}
-		// }
+	if (message.author.bot) return;
+	if (!message.content.startsWith('!')) return;
+
+	var userInput = message.content.substr(1).toLocaleLowerCase();
+	var command = userInput.split(' ')[0];
+	var args = userInput.split(' ')[1];
+
+	if (command == 'time') {
+		var user = getUserFromMention(args) || message.author;
+
+		if (!user.bot) {
+			updateTime(user.id);
+
+			var time = msToTime(JSONdata[user.id].timeInVC);
+			var responce = `${user.username} has been in VC for: `;
+
+			if (time[0] != 0) responce += `**${time[0]}** days, `;
+			if (time[1] != 0) responce += `**${time[1]}** hours, `;
+			if (time[2] != 0) responce += `**${time[2]}** minutes and `;
+			responce += `**${time[3]}** seconds! `;
+			if (time[3] == 0) responce += 'How sad :(';
+
+			message.channel.send(responce);
+		} else message.channel.send('Yo why you bringing bots into this?');
 	}
 });
